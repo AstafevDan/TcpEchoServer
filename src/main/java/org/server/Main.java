63fe130c994427a5075c0ba66439804921c1382d
Daniil <astafiev.den@gmail.com>
2025-02-01 18:51:12 +0300

    added simple multithreading tcp echo server implementation

--- src/main/java/org/server/Main.java
+++ src/main/java/org/server/Main.java
@@ -1,7 +1,12 @@
 package org.server;
 
+/**
+ * В этом классе в методе main создается экземпляр сервера с указанием порта
+ * и количества потоков, которое будет создано в пуле.
+ */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        TcpEchoServer server = new TcpEchoServer(7777, 15);
+        server.run();
     }
 }
